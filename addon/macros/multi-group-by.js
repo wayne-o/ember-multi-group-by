4a91379bf67285b73fa09d08d1779fcd35053fd9
wayne-o <wayne@prjatk.com>
2016-06-06 09:34:04 +0100

    - reference original object else users will need to refetch - bump version

--- addon/macros/multi-group-by.js
+++ addon/macros/multi-group-by.js
@@ -1,12 +1,12 @@
-var DataGrouper = (function() {
-	var has = function(obj, target) {
-		return _.any(obj, function(value) {
+var DataGrouper = (function () {
+	var has = function (obj, target) {
+		return _.any(obj, function (value) {
 			return _.isEqual(value, target);
 		});
 	};
 
-	var keys = function(data, names) {
-		return _.reduce(data, function(memo, item) {
+	var keys = function (data, names) {
+		return _.reduce(data, function (memo, item) {
 			var key = _.pick(item, names);
 			if (!has(memo, key)) {
 				memo.push(key);
@@ -15,30 +15,30 @@ var DataGrouper = (function() {
 		}, []);
 	};
 
-	var group = function(data, names) {
+	var group = function (data, names) {
 		var stems = keys(data, names);
-		return _.map(stems, function(stem) {
+		return _.map(stems, function (stem) {
 			return {
 				key: stem,
-				vals: _.map(_.where(data, stem), function(item) {
+				vals: _.map(_.where(data, stem), function (item) {
 					return _.omit(item, names);
 				})
 			};
 		});
 	};
 
-	group.register = function(name, converter) {
-		return group[name] = function(data, names) {
+	group.register = function (name, converter) {
+		return group[name] = function (data, names) {
 			return _.map(group(data, names), converter);
 		};
 	};
 
 	return group;
-}());
+} ());
 
-DataGrouper.register("sum", function(item) {
+DataGrouper.register("sum", function (item) {
 	return _.extend({}, item.key, {
-		Value: _.reduce(item.vals, function(memo, node) {
+		Value: _.reduce(item.vals, function (memo, node) {
 			return memo + Number(node.Value);
 		}, 0)
 	});
@@ -48,8 +48,10 @@ export default function multiGroupBy(collection, properties, context) {
 	var data = context.get(collection)
 		.toArray();
 
-	var mapped = _.map(data, function(x) {
-		return x.toJSON();
+	var mapped = _.map(data, function (x) {
+		var ret = x.toJSON({ includeId: true });
+		ret.original = x;
+		return ret;
 	});
 
 	var dg = new DataGrouper(mapped, properties);
